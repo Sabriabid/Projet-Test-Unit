219c47fc86c97298fc0d5cb4ccdcf872d466662a
Rayane HAROUN <rharoun@layer-data.com>
2021-12-09 12:19:33 +0100

    Test Date

--- test_server.py
+++ test_server.py
@@ -17,7 +17,8 @@ def test_showSummary(client):
     assert rv.status_code == 200
     data = rv.data.decode()
     assert "Welcome, john@simplylift.co" in data
-
+    
+#Test adresse mail non indetifiée
 def test_NoShowSummary(client):
      rv = client.post('/showSummary', data=dict(email="john@simplylift.com"), follow_redirects=True)
      assert rv.status_code == 400
@@ -25,18 +26,21 @@ def test_NoShowSummary(client):
      assert "404" in data
      assert "PAGE NOT FOUND" in data
 
+#test du booking
 def test_booking(client):
     rv= client.post('/purchasePlaces', data =dict(club = 'Simply Lift', competition='Spring Festival', places = 5), follow_redirects = True)
     assert rv.status_code == 200
     data = rv.data.decode()
     assert "/book/Fall%20Classic/Simply%20Lift" in data
-    
+
+#Test de nobooking
 def test_nobooking(client):
     rv= client.get("/book/Spring%20Festival/Simply%20Lift1", follow_redirects=True)
     assert rv.status_code == 400
     data = rv.data.decode()
     assert "PAGE NOT FOUND" in data
-  
+
+#Test du bon deroulement de l'achat
 def test_purchase(client):
     rv2=client.post('/purchasePlaces', data =dict(club = "Simply Lift", competition="Spring Festival", places = 2), follow_redirects = True)
     assert rv2.status_code == 200
@@ -46,7 +50,8 @@ def test_purchase(client):
     assert "Number of Places: 23" in data 
     assert "Points available: 7" in data 
 
-def test_purchase12(client):
+#Test de la limite des 12 places achetées 
+def test_purchase_12places(client):
     rv2=client.post('/purchasePlaces', data =dict(club = "Simply Lift", competition="Spring Festival", places = 12), follow_redirects = True)
     assert rv2.status_code == 200
     data = rv2.data.decode()
@@ -58,18 +63,29 @@ def test_purchase12(client):
     # assert "Number of Places: 25" in data 
     # assert "Points available: 13" in data
 
-
+#Test de la disponibilité des points lors de l'achat
 def test_purchase_PointDispo(client):
     rv2=client.post('/purchasePlaces', data =dict(club = "Simply Lift", competition="Spring Festival", places = 10), follow_redirects = True)
     assert rv2.status_code == 200
     data = rv2.data.decode()
-    print(data)
     assert data.find("Not enought points !")
     #pytest
     assert "Number of Places: 23" in data 
     assert "Points available: 7" in data
     #ligne de commande:*
     assert "Number of Places: 25" in data 
+    assert "Points available: 13" in data
+# Test de Date de competition 
+def test_datecompetition(client):
+    rv=client.post('/purchasePlaces', data =dict(club = "Simply Lift", competition="Fall Classic", places = 1), follow_redirects = True)
+    assert rv.status_code ==200
+    data= rv.data.decode()
+    assert data.find("La competition est deja passé")
+     #pytest
+    # assert "Number of Places: 11" in data 
+    # assert "Points available: 7" in data
+    #ligne de commande:*
+    assert "Number of Places: 13" in data 
     assert "Points available: 13" in data
 
 def test_logout(client):
@@ -79,28 +95,34 @@ def test_logout(client):
     assert data.find("Welcome to the GUDLFT Registration Portal!")
 
 def test_integration(client):
+    #Load Club
     club= loadClubs()
     assert list(club[0].keys())==['name','email','points']
+    #Load Compet
     competitions = loadCompetitions()
     assert list(competitions[0].keys())==['name','date','numberOfPlaces']
 
+    #index
     rp = client.get('/')
     assert rp.status_code == 200
     data=rp.data.decode()
     print(data)
     assert "Welcome to the GUDLFT Registration Portal!" in data
 
+    #Connexion
     rv= client.post('/showSummary',data=dict(email="john@simplylift.co"),follow_redirects=True)
     assert rv.status_code == 200
     data1 = rv.data.decode()
     assert "/book/Fall%20Classic/Simply%20Lift" in data1
 
+    #Acces a la page d'achat
     rv_test= client.get('/book/Spring%20Festival/Simply%20Lift',follow_redirects=True)
     assert rv_test.status_code==200
     data2=rv_test.data.decode()
     print(data1)
     assert "Spring Festival" in data2
 
+    #Achat
     rv_test1=client.post('/purchasePlaces', data =dict(club="Simply Lift", competition="Spring Festival", places= 1), follow_redirects = True)
     assert rv_test1.status_code == 200
     data3 =rv_test1.data.decode()
@@ -113,7 +135,7 @@ def test_integration(client):
     assert "Number of Places: 24" in data3
     assert "Points available: 10" in data3
     
-
+    #Deconnexion
     rv_logout = client.get("/logout", follow_redirects=True)
     assert rv_logout.status_code == 200
     data4= rv_logout.data.decode()
